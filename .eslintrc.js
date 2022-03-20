646e42da27a05537a9ec276c034f3d6f0a0c9b1c
Trezy <t@trezy.com>
2022-03-20 11:17:18 -0500

    eslint!

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2022: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:editorconfig/all',
+		'plugin:jsdoc/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:optimize-regex/recommended',
+		'plugin:promise/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:react-perf/recommended',
+		'plugin:security/recommended',
+	],
+	plugins: [
+		'editorconfig',
+		'sort-class-members',
+		'unused-imports',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	rules: {
+		'no-unused-vars': 'error',
+		'sort-class-members/sort-class-members': ['error', {
+			accessorPairPositioning: 'getThenSet',
+			order: [
+				'[static-properties]',
+				'[static-methods]',
+				'[conventional-private-properties]',
+				'[properties]',
+				'constructor',
+				'[methods]',
+				'[conventional-private-methods]',
+			],
+		}],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
