bbd59c30f140cde1cf1eb753e739fb91f913c2a4
Trezy <t@trezy.com>
2023-01-14 23:49:40 -0600

    abstract map editor controls

---
+++ src/components/MapEditor/EditorControls/EditorControls.jsx
@@ -0,0 +1,96 @@
+// Local imports
+import styles from './EditorControls.module.scss'
+
+import { Button } from '../../Button.jsx'
+import { useEditor } from '../../scenes/Architect/context/EditorContext.jsx'
+
+
+
+
+
+/**
+ * Renders a canvas for painting tiles to build a map.
+ */
+export function EditorControls() {
+	const {
+		activateBrushTool,
+		activateEraserTool,
+		activateHandTool,
+		activateMarqueeTool,
+		defaultZoom,
+		tool,
+		zoom,
+		zoomIn,
+		zoomOut,
+	} = useEditor()
+
+	return (
+		<menu
+			className={'editor-controls'}
+			type={'toolbar'}>
+			<div className={'control'}>
+				<label>{'Tools'}</label>
+
+				<div className={'button-group'}>
+					<Button
+						isAffirmative={tool === 'hand'}
+						isUniformlyPadded
+						onClick={activateHandTool}>
+						<img
+							alt={'Hand Tool'}
+							src={'/static/assets/cursors/hand.png'} />
+					</Button>
+
+					<Button
+						isAffirmative={tool === 'eraser'}
+						isUniformlyPadded
+						onClick={activateEraserTool}>
+						<img
+							alt={'Eraser Tool'}
+							src={'/static/assets/cursors/eraser.png'} />
+					</Button>
+
+					<Button
+						isAffirmative={tool === 'marquee'}
+						isUniformlyPadded
+						onClick={activateMarqueeTool}>
+						<img
+							alt={'Marquee Tool'}
+							src={'/static/assets/cursors/marquee.png'} />
+					</Button>
+
+					<Button
+						isAffirmative={tool === 'brush'}
+						isUniformlyPadded
+						onClick={activateBrushTool}>
+						<img
+							alt={'Brush Tool'}
+							src={'/static/assets/cursors/brush.png'} />
+					</Button>
+				</div>
+			</div>
+
+			<div className={'control'}>
+				<label>{'Zoom'}</label>
+
+				<div className={'button-group'}>
+					<Button
+						isUniformlyPadded
+						onClick={zoomOut}>
+						{'-'}
+					</Button>
+
+					<div>
+						{`${(zoom - defaultZoom + 1) * 100}%`}
+					</div>
+
+					<Button
+						isUniformlyPadded
+						onClick={zoomIn}>
+						{'+'}
+					</Button>
+				</div>
+			</div>
+		</menu>
+	)
+}
--- src/components/MapEditor/MapEditorCanvas.jsx
+++ src/components/MapEditor/MapEditorCanvas.jsx
@@ -1,7 +1,6 @@
 // Local imports
-import { Button } from '../Button.jsx'
 import { Editor } from '../scenes/Architect/Editor.jsx'
-import { useEditor } from '../scenes/Architect/context/EditorContext.jsx'
+import { EditorControls } from './EditorControls/EditorControls.jsx'
 
 
 
@@ -11,91 +10,13 @@ import { useEditor } from '../scenes/Architect/context/EditorContext.jsx'
  * Renders a canvas for painting tiles to build a map.
  */
 export function MapEditorCanvas() {
-	const {
-		activateBrushTool,
-		activateEraserTool,
-		activateHandTool,
-		activateMarqueeTool,
-		defaultZoom,
-		tool,
-		zoom,
-		zoomIn,
-		zoomOut,
-	} = useEditor()
-
 	return (
 		<div className={'editor-container'}>
 			<Editor
 				showMapGrid
 				showTransparencyGrid={false} />
 
-			<menu
-				className={'editor-controls'}
-				type={'toolbar'}>
-				<div className={'control'}>
-					<label>{'Tools'}</label>
-
-					<div className={'button-group'}>
-						<Button
-							isAffirmative={tool === 'hand'}
-							isUniformlyPadded
-							onClick={activateHandTool}>
-							<img
-								alt={'Hand Tool'}
-								src={'/static/assets/cursors/hand.png'} />
-						</Button>
-
-						<Button
-							isAffirmative={tool === 'eraser'}
-							isUniformlyPadded
-							onClick={activateEraserTool}>
-							<img
-								alt={'Eraser Tool'}
-								src={'/static/assets/cursors/eraser.png'} />
-						</Button>
-
-						<Button
-							isAffirmative={tool === 'marquee'}
-							isUniformlyPadded
-							onClick={activateMarqueeTool}>
-							<img
-								alt={'Marquee Tool'}
-								src={'/static/assets/cursors/marquee.png'} />
-						</Button>
-
-						<Button
-							isAffirmative={tool === 'brush'}
-							isUniformlyPadded
-							onClick={activateBrushTool}>
-							<img
-								alt={'Brush Tool'}
-								src={'/static/assets/cursors/brush.png'} />
-						</Button>
-					</div>
-				</div>
-
-				<div className={'control'}>
-					<label>{'Zoom'}</label>
-
-					<div className={'button-group'}>
-						<Button
-							isUniformlyPadded
-							onClick={zoomOut}>
-							{'-'}
-						</Button>
-
-						<div>
-							{`${(zoom - defaultZoom + 1) * 100}%`}
-						</div>
-
-						<Button
-							isUniformlyPadded
-							onClick={zoomIn}>
-							{'+'}
-						</Button>
-					</div>
-				</div>
-			</menu>
+			<EditorControls />
 		</div>
 	)
 }
