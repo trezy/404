67397b4c3b3120522c086dce4d2f85f90a5f789c
Trezy <t@trezy.com>
2023-01-18 16:46:47 -0600

    update map card styles

--- src/components/scenes/CustomGame/MapCard.jsx
+++ src/components/scenes/CustomGame/MapCard.jsx
@@ -31,27 +31,29 @@ export function MapCard(props) {
 					src={'https://via.placeholder.com/267x150'} />
 			</div>
 
-			<header className={styles['name']}>
-				{map.name}
-			</header>
+			<div className={styles['details']}>
+				<header className={styles['name']}>
+					{map.name}
+				</header>
 
-			<div className={styles['description']}>
-				<p>{'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar rutrum erat, eget luctus magna.'}</p>
+				<div className={styles['description']}>
+					<p>{'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar rutrum erat, eget luctus magna.'}</p>
+				</div>
+
+				<dl className={styles['stats']}>
+					<dt>{'Downloads'}</dt>
+					<dd>{'1234'}</dd>
+
+					<dt>{'High Score'}</dt>
+					<dd>{'450000'}</dd>
+
+					<dt>{'Size'}</dt>
+					<dd>{numeral(map.size).format('0.00 b')}</dd>
+
+					<dt>{'Version'}</dt>
+					<dd>{map.version}</dd>
+				</dl>
 			</div>
-
-			<dl className={styles['stats']}>
-				<dt>{'Downloads'}</dt>
-				<dd>{'1234'}</dd>
-
-				<dt>{'High Score'}</dt>
-				<dd>{'450000'}</dd>
-
-				<dt>{'Size'}</dt>
-				<dd>{numeral(map.size).format('0.00 b')}</dd>
-
-				<dt>{'Version'}</dt>
-				<dd>{map.version}</dd>
-			</dl>
 		</li>
 	)
 }
